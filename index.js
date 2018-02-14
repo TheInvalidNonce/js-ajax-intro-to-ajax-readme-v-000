2394047b1ff860d3ae5886431da0c39820dac59e
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-14 19:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function showRepositories(event, data) {
   console.log(this.responseText)
+  
+  let repoList = "<ul>"
+  
+  for (let i in responseText) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  
+  repoList+= "</ul>"
+  
+  document.getElementById('repositories').innerHTML = repoList
 }
 
 
