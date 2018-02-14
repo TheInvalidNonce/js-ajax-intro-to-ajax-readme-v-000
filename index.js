0ee72476666563f5dfa4623ea99147ccf5a8d91e
Janusz Szubert <theinvalidnonce@gmail.com>
2018-02-14 19:25:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function showRepositories(event, data) {
   document.getElementById('repositories').innerHTML = repoList
 }
 
-
 function getRepositories() {
   const req = new XMLHttpRequest();
   
@@ -15,4 +14,13 @@ function getRepositories() {
   
   req.open("GET", 'https://api.github.com/users/octocat/repos');
   req.send()
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest()
+  
+  req.addEventListener("load", showCommits);
+  
+  req.open()
 }
